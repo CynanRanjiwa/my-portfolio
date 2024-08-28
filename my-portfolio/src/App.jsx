4bd372cc9dc3b7d3c5e8e84f813562879aff931f
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-08-28 04:50:15 +0300

    updating the App.jsx

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
+import Header from './components/Header';
+import ProjectList from './components/ProjectList';
+import ContactForm from './components/ContactForm';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';// importing the css file for styling 
 
-// a simple functional component for the Header 
-function Header() {
-  return (
-    <header className="header">
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is a showcase of my work and projects.</p>
 
-    </header>
-  );
-}
-
-//main app component where other components will be integrated 
 function App() {
   return (
-    <div className="App">
-      <Header/> {/* Including the Header component */}
-      {/* Other components like project showcase will go here */}
-      <section>
-        <h2>About Me</h2>
-        <p>Hi! I'm a full-stack developer with a passion for building web applications.</p>
-      </section>
-
-    </div>
+    <Router>
+      <div className="App">
+        <Header /> {/* Including the Header component */}
+        {/* React Router setup to manage routes */}
+        <Routes>
+          <Route path="/" element={<About />} /> {/* Default route to the About page */}
+          <Route path="/projects" element={<ProjectList />} /> {/* Route to the ProjectList page */}
+          <Route path="/contact" element={<ContactForm />} /> {/* Route to the ContactForm page */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+export default App; // Exporting the App component
